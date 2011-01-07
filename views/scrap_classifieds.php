2f980c57dedd3352da1f6a951f11e33f837d46ac
Greg Crown <ww4667@gmail.com>
2011-01-07 13:20:34 -0600

    classifieds updated, again...

--- views/scrap_classifieds.php
+++ views/scrap_classifieds.php
@@ -12,7 +12,12 @@
 		}, function(){ 
 	       $(this).attr('src', '/resources/images/buttons/post_a_classified.png'); 
 		});
-			
+   
+   sw.tabOneSlider = new sw.app.verticalSlider('#tab1', '#tab-1-pane','#tab-1-content',{overflow: "hidden", float: "left", height: "645px", width: "547px"}, {position: "relative"} );
+   sw.tabTwoSlider = new sw.app.verticalSlider('#tab2', '#tab-2-pane','#tab-2-content',{overflow: "hidden", float: "left", height: "645px", width: "547px"}, {position: "relative"} );    
+   sw.tabOneSlider = new sw.app.verticalSlider('#tab3', '#tab-3-pane','#tab-3-content',{overflow: "hidden", float: "left", height: "645px", width: "547px"}, {position: "relative"} );
+   sw.tabTwoSlider = new sw.app.verticalSlider('#tab4', '#tab-4-pane','#tab-4-content',{overflow: "hidden", float: "left", height: "645px", width: "547px"}, {position: "relative"} );
+  
   });
   </script>
 
@@ -31,35 +36,37 @@
 	</ul>
 	
 	<div class="tabBox">
+		
 		<div id="tab1">
-			<div id="scroll-pane1">
-				
-				[[Ditto? &parents=`36`&orderBy=`publishedon ASC` &display=`500` &total=`500` &tpl=`scrap_row_white` &tplAlt=`scrap_row_gray`]]
-				
-			</div><!-- scroll-pane1 -->
+    <div id="tab-1-pane">
+      <div id="tab-1-content">
+        [[Ditto? &parents=`36`&orderBy=`publishedon ASC` &display=`500` &total=`500` &tpl=`scrap_row_white` &tplAlt=`scrap_row_gray`]]
+        </div><!-- tab1 conent -->
+    </div><!-- tab1 pane -->
 		</div><!-- tab1 -->
 		
 		<div id="tab2">
-			<div id="scroll-pane2">
-
-				[[Ditto? &parents=`37`&orderBy=`publishedon ASC` &display=`500` &total=`500` &tpl=`scrap_row_white` &tplAlt=`scrap_row_gray`]]
-												
-			</div><!-- scroll-pane2 -->
+    <div id="tab-2-pane">
+      <div id="tab-2-content">
+		[[Ditto? &parents=`37`&orderBy=`publishedon ASC` &display=`500` &total=`500` &tpl=`scrap_row_white` &tplAlt=`scrap_row_gray`]]
+        </div><!-- tab2 conent -->
+    </div><!-- tab2 pane -->
 		</div><!-- tab2 -->
+		
 		<div id="tab3">
-			<div id="scroll-pane3">
-				
-				[[Ditto? &parents=`51`&orderBy=`publishedon ASC` &display=`500` &total=`500` &tpl=`scrap_row_white` &tplAlt=`scrap_row_gray`]]
-				
-			</div><!-- scroll-pane1 -->
+    <div id="tab-3-pane">
+      <div id="tab-3-content">
+		[[Ditto? &parents=`51`&orderBy=`publishedon ASC` &display=`500` &total=`500` &tpl=`scrap_row_white` &tplAlt=`scrap_row_gray`]]
+        </div><!-- tab3 conent -->
+    </div><!-- tab3 pane -->
 		</div><!-- tab3 -->
 		
 		<div id="tab4">
-			<div id="scroll-pane4">
-
-				[[Ditto? &parents=`53`&orderBy=`publishedon ASC` &display=`500` &total=`500` &tpl=`scrap_row_white` &tplAlt=`scrap_row_gray`]]
-												
-			</div><!-- scroll-pane2 -->
+    <div id="tab-4-pane">
+      <div id="tab-4-content">
+		[[Ditto? &parents=`53`&orderBy=`publishedon ASC` &display=`500` &total=`500` &tpl=`scrap_row_white` &tplAlt=`scrap_row_gray`]]
+        </div><!-- tab4 conent -->
+    </div><!-- tab4 pane -->
 		</div><!-- tab4 -->
 			
 	</div><!-- tabBox -->
