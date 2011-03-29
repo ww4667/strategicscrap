d26ff8dfb43ff9f16b5fa79d61ffc38893d498ff
Greg Crown <ww4667@gmail.com>
2011-03-29 02:09:37 -0500

    make youtube links SSL

--- views/scrap_classifieds.php
+++ views/scrap_classifieds.php
@@ -33,7 +33,7 @@
 
 <div id="scrapClassifieds" class="classifiedListing">
 	<div>
-		<div style="float:left"><a class="watch_video" href="http://www.youtube.com/embed/1kIim6cMN_w"><img src="/resources/images/buttons/watch_video.png" alt="watch a video" id="watch_video" /></a></div>
+		<div style="float:left"><a class="watch_video" href="https://www.youtube.com/embed/1kIim6cMN_w"><img src="/resources/images/buttons/watch_video.png" alt="watch a video" id="watch_video" /></a></div>
 		<div style="float:left;margin-left:20px"><a href="mailto:classifieds@strategicscrap.com?subject=I would like to post a scrap classified&body=Hello, Below are the details of my classified ad..."><img src="/resources/images/buttons/post_a_classified.png" alt="post a classified"  id="post_a_classified" /></a></div>
 		<div style="clear:both;padding:20px 0">If you would like us to post your classified, please call us at 866-796-7272 or email us at classifieds@strategicscrap.com</div>
 	</div>
